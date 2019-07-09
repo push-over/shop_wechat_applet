361ecd52c736d0f9e82f2e9224ed7253cb05afef
高赞 <gaozan@anchnet.com>
2019-07-09 12:03:24 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
